ca1adb68714af7c526aa68071055ff47298d891b
Eduardo Eitelven <dudaeitelven@gmail.com>
2019-10-06 16:40:56 -0300

    2

--- Airport/src/principal/Main.java
+++ Airport/src/principal/Main.java
@@ -7,54 +7,54 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Aviao> aHangar = new ArrayList();
-		ArrayList<Aviao> aPistaSul = new ArrayList();
-		ArrayList<Aviao> aPistaNorte = new ArrayList();
-		ArrayList<Aviao> aAirway10000 = new ArrayList();
-		ArrayList<Aviao> aAirway15000 = new ArrayList();
-		ArrayList<Aviao> aAirway20000 = new ArrayList();
+		ArrayList<Aviao> aHangar =      new ArrayList<Aviao>();
+		ArrayList<Aviao> aPistaSul =    new ArrayList<Aviao>();
+		ArrayList<Aviao> aPistaNorte =  new ArrayList<Aviao>();
+		ArrayList<Aviao> aAirway10000 = new ArrayList<Aviao>();
+		ArrayList<Aviao> aAirway15000 = new ArrayList<Aviao>();
+		ArrayList<Aviao> aAirway20000 = new ArrayList<Aviao>();
 
 		Aviao av1 = new Aviao(1);
 		Aviao av2 = new Aviao(2);
 		Aviao av3 = new Aviao(3);
 		Aviao av4 = new Aviao(4);
 		Aviao av5 = new Aviao(5);
-		Aviao av6 = new Aviao(6);
-		Aviao av7 = new Aviao(7);
-		Aviao av8 = new Aviao(8);
-		Aviao av9 = new Aviao(9);
-		Aviao av10 = new Aviao(10);
-		Aviao av11 = new Aviao(11);
-		Aviao av12 = new Aviao(12);
-		Aviao av13 = new Aviao(13);
-		Aviao av14 = new Aviao(14);
-		Aviao av15 = new Aviao(15);
-		Aviao av16 = new Aviao(16);
-		Aviao av17 = new Aviao(17);
-		Aviao av18 = new Aviao(18);
-		Aviao av19 = new Aviao(19);
-		Aviao av20 = new Aviao(20);
+//		Aviao av6 = new Aviao(6);
+//		Aviao av7 = new Aviao(7);
+//		Aviao av8 = new Aviao(8);
+//		Aviao av9 = new Aviao(9);
+//		Aviao av10 = new Aviao(10);
+//		Aviao av11 = new Aviao(11);
+//		Aviao av12 = new Aviao(12);
+//		Aviao av13 = new Aviao(13);
+//		Aviao av14 = new Aviao(14);
+//		Aviao av15 = new Aviao(15);
+//		Aviao av16 = new Aviao(16);
+//		Aviao av17 = new Aviao(17);
+//		Aviao av18 = new Aviao(18);
+//		Aviao av19 = new Aviao(19);
+//		Aviao av20 = new Aviao(20);
 
 		aHangar.add(av1);
 		aHangar.add(av2);
 		aHangar.add(av3);
 		aHangar.add(av4);
 		aHangar.add(av5);
-		aHangar.add(av6);
-		aHangar.add(av7);
-		aHangar.add(av8);
-		aHangar.add(av9);
-		aHangar.add(av10);
-		aHangar.add(av11);
-		aHangar.add(av12);
-		aHangar.add(av13);
-		aHangar.add(av14);
-		aHangar.add(av15);
-		aHangar.add(av16);
-		aHangar.add(av17);
-		aHangar.add(av18);
-		aHangar.add(av19);
-		aHangar.add(av20);
+//		aHangar.add(av6);
+//		aHangar.add(av7);
+//		aHangar.add(av8);
+//		aHangar.add(av9);
+//		aHangar.add(av10);
+//		aHangar.add(av11);
+//		aHangar.add(av12);
+//		aHangar.add(av13);
+//		aHangar.add(av14);
+//		aHangar.add(av15);
+//		aHangar.add(av16);
+//		aHangar.add(av17);
+//		aHangar.add(av18);
+//		aHangar.add(av19);
+//		aHangar.add(av20);
 
 
 		/* Thread decolagem */
@@ -119,15 +119,63 @@ public class Main {
 			}
 		}.start();
 		
+		/* Thread Way15000 */
+		new Thread() {
 
-		for (Aviao aPista : aPistaSul) {
-			System.out.println(aPista.getIdentificador());
-		}
-		System.out.println("--------");
+			@Override
+			public void run() {
+				while ((aAirway10000.size()-1)!= 0) {
+					try {
+						Thread.sleep(3000);
+
+						
+						aAirway15000.add(aAirway10000.get(0));
+						aAirway10000.remove(0);
+						
+						System.out.println("--Airway15000--");
+						
+						for (Aviao aAirway : aAirway15000) {
+							System.out.println(aAirway.getIdentificador());
+						}
+						
+						System.out.println("--------");
+						
+					} catch (InterruptedException e) {
+					}
+
+				}
+
+			}
+		}.start();
 		
-		for (Aviao av: aHangar) {
-			System.out.println(av.getIdentificador());	
-		}
+		/* Thread Way20000 */
+		new Thread() {
+
+			@Override
+			public void run() {
+				while ((aAirway15000.size()-1)!= 0) {
+					try {
+						Thread.sleep(4000);
+
+						
+						aAirway20000.add(aAirway15000.get(0));
+						aAirway15000.remove(0);
+						
+						System.out.println("--Airway20000--");
+						
+						for (Aviao aAirway : aAirway20000) {
+							System.out.println(aAirway.getIdentificador());
+						}
+						
+						System.out.println("--------");
+						
+					} catch (InterruptedException e) {
+					}
+
+				}
+
+			}
+		}.start();
 		
 
 	}
