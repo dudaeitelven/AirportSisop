d6b477503c9ca6a2cf705995e776d343b43361ea
Eduardo <Eduardo@DESKTOP-HJSI9IN>
2019-10-08 22:45:43 -0300

    4

--- Airport/src/principal/GerenciarAvioes.java
+++ Airport/src/principal/GerenciarAvioes.java
@@ -71,15 +71,17 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(3000);
-
-						if ((aAirway10000.size() - 1) >= 0) {
-
-							for (int i = 0; i < aAirway10000.size(); i++) {
-								if (aAirway10000.get(i).getSubir() == true) {
-									aAirway10000.get(i).setTempoSubir(200);
-									aAirway10000.get(i).setSubir(false);
-									aAirway15000.add(aAirway10000.get(i));
-									aAirway10000.remove(i);
+						
+						synchronized(this) {
+							if ((aAirway10000.size() - 1) >= 0) {
+	
+								for (int i = 0; i < aAirway10000.size(); i++) {
+									if (aAirway10000.get(i).getSubir() == true) {
+										aAirway10000.get(i).setTempoSubir(200);
+										aAirway10000.get(i).setSubir(false);
+										aAirway15000.add(aAirway10000.get(i));
+										aAirway10000.remove(i);
+									}
 								}
 							}
 						}
@@ -95,14 +97,16 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(4000);
-
-						if ((aAirway15000.size() - 1) >= 0) {
-							for (int i = 0; i < aAirway15000.size(); i++) {
-								if (aAirway15000.get(i).getSubir() == true) {
-									aAirway15000.get(i).setTempoSubir(200);
-									aAirway15000.get(i).setSubir(false);
-									aAirway20000.add(aAirway15000.get(i));
-									aAirway15000.remove(i);
+						
+						synchronized(this) {
+							if ((aAirway15000.size() - 1) >= 0) {
+								for (int i = 0; i < aAirway15000.size(); i++) {
+									if (aAirway15000.get(i).getSubir() == true) {
+										aAirway15000.get(i).setTempoSubir(200);
+										aAirway15000.get(i).setSubir(false);
+										aAirway20000.add(aAirway15000.get(i));
+										aAirway15000.remove(i);
+									}
 								}
 							}
 						}
@@ -118,15 +122,17 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(5000);
-
-						if ((aAirway20000.size() - 1) >= 0) {
-							for (int i = 0; i < aAirway20000.size(); i++) {
-								if (aAirway20000.get(i).getDescer() == true) {
-									aAirway20000.get(i).setTempoDescer(200);
-									aAirway20000.get(i).setDescer(false);
-									aAirway15000.add(aAirway20000.get(i));
-									aAirway20000.remove(i);
-									// break;
+						
+						synchronized(this) {
+							if ((aAirway20000.size() - 1) >= 0) {
+								for (int i = 0; i < aAirway20000.size(); i++) {
+									if (aAirway20000.get(i).getDescer() == true) {
+										aAirway20000.get(i).setTempoDescer(200);
+										aAirway20000.get(i).setDescer(false);
+										aAirway15000.add(aAirway20000.get(i));
+										aAirway20000.remove(i);
+										// break;
+									}
 								}
 							}
 						}
@@ -142,15 +148,17 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(6000);
-
-						if ((aAirway15000.size() - 1) >= 0) {
-							for (int i = 0; i < aAirway15000.size(); i++) {
-								if (aAirway15000.get(i).getDescer() == true) {
-									aAirway15000.get(i).setTempoDescer(200);
-									aAirway15000.get(i).setDescer(false);
-									aAirway10000.add(aAirway15000.get(i));
-									aAirway15000.remove(i);
-									// break;
+						
+						synchronized(this) {
+							if ((aAirway15000.size() - 1) >= 0) {
+								for (int i = 0; i < aAirway15000.size(); i++) {
+									if (aAirway15000.get(i).getDescer() == true) {
+										aAirway15000.get(i).setTempoDescer(200);
+										aAirway15000.get(i).setDescer(false);
+										aAirway10000.add(aAirway15000.get(i));
+										aAirway15000.remove(i);
+										// break;
+									}
 								}
 							}
 						}
@@ -166,15 +174,17 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(7000);
-
-						if ((aAirway10000.size() - 1) >= 0) {
-							for (int i = 0; i < aAirway10000.size(); i++) {
-								if (aAirway10000.get(i).getDescer() == true) {
-									aAirway10000.get(i).setTempoDescer(200);
-									aAirway10000.get(i).setDescer(false);
-									aFilaAterrissar.add(aAirway10000.get(i));
-									aAirway10000.remove(i);
-									// break;
+						
+						synchronized(this) {
+							if ((aAirway10000.size() - 1) >= 0) {
+								for (int i = 0; i < aAirway10000.size(); i++) {
+									if (aAirway10000.get(i).getDescer() == true) {
+										aAirway10000.get(i).setTempoDescer(200);
+										aAirway10000.get(i).setDescer(false);
+										aFilaAterrissar.add(aAirway10000.get(i));
+										aAirway10000.remove(i);
+										// break;
+									}
 								}
 							}
 						}
@@ -191,12 +201,14 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(3000);
-
-						if ((aFilaAterrissar.size() - 1) >= 0) {
-							aHangar.add(aFilaAterrissar.get(0));
-							aFilaAterrissar.remove(0);
-							
-						}				
+						
+						synchronized(this) {
+							if ((aFilaAterrissar.size() - 1) >= 0) {
+								aHangar.add(aFilaAterrissar.get(0));
+								aFilaAterrissar.remove(0);
+								
+							}		
+						}
 					} catch (InterruptedException e) {}
 				}
 			}
@@ -209,37 +221,37 @@ public class GerenciarAvioes {
 				while (true) {
 					try {
 						Thread.sleep(500);
-
-						System.out.println("\n--HANGAR--");
-						for (Aviao aHangar : aHangar) {
-							System.out.print(aHangar.getIdentificador() + " ");
+						
+						synchronized(this) {
+							System.out.println("\n--HANGAR--");
+							for (Aviao aHangar : aHangar) {
+								System.out.print(aHangar.getIdentificador() + " ");
+							}
+							System.out.println("\n--DECOLAGEM--");
+							for (Aviao aPista : aFilaDecolagem) {
+								System.out.print(aPista.getIdentificador() + " ");
+							}
+							System.out.println("\n--Aterrissagem--");
+							for (Aviao aPista : aFilaAterrissar) {
+								System.out.print(aPista.getIdentificador() + " ");
+							}
+							System.out.println("\n--AIRWAY 10000--");
+	
+							for (Aviao aWay1000 : aAirway10000) {
+								System.out.print(aWay1000.getIdentificador() + " ");
+							}
+							System.out.println("\n--AIRWAY 15000--");
+							for (Aviao aWay1000 : aAirway15000) {
+								System.out.print(aWay1000.getIdentificador() + " ");
+							}
+							System.out.println("\n--AIRWAY 20000--");
+							for (Aviao aWay1000 : aAirway20000) {
+								System.out.print(aWay1000.getIdentificador() + " ");
+							}
+	
+							System.out.println("\n--------");
 						}
-						System.out.println("\n--DECOLAGEM--");
-						for (Aviao aPista : aFilaDecolagem) {
-							System.out.print(aPista.getIdentificador() + " ");
-						}
-						System.out.println("\n--Aterrissagem--");
-						for (Aviao aPista : aFilaAterrissar) {
-							System.out.print(aPista.getIdentificador() + " ");
-						}
-						System.out.println("\n--AIRWAY 10000--");
-
-						for (Aviao aWay1000 : aAirway10000) {
-							System.out.print(aWay1000.getIdentificador() + " ");
-						}
-						System.out.println("\n--AIRWAY 15000--");
-						for (Aviao aWay1000 : aAirway15000) {
-							System.out.print(aWay1000.getIdentificador() + " ");
-						}
-						System.out.println("\n--AIRWAY 20000--");
-						for (Aviao aWay1000 : aAirway20000) {
-							System.out.print(aWay1000.getIdentificador() + " ");
-						}
-
-						System.out.println("\n--------");
-
-					} catch (InterruptedException e) {
-					}
+					} catch (InterruptedException e) {}
 				}
 			}
 		};
